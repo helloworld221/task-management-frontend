79a1376904fad0a78490bdac4e0d0f2c7685e2ef
helloworld221 <helloworld221@users.noreply.github.com>
2022-11-24 06:43:49 +0000

    deploy: 6931a8d12a43b868d9a50b2d554b141626f3b813

---
+++ precache-manifest.e7a2ec718243555a10706ceb284bceaf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa97f0f682a983a8a56dca23af8ea8f",
+    "url": "https://helloworld221.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "2fc4e478fb39665f3287",
+    "url": "https://helloworld221.github.io/task-management-frontend/static/js/2.23154354.chunk.js"
+  },
+  {
+    "revision": "20bc26d831d2ce79638c",
+    "url": "https://helloworld221.github.io/task-management-frontend/static/js/main.389a6241.chunk.js"
+  },
+  {
+    "revision": "184c1a7ccb14d75a9241",
+    "url": "https://helloworld221.github.io/task-management-frontend/static/js/runtime~main.049d98bc.js"
+  }
+]);
